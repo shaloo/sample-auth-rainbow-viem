4ece1e97dfd8d0cb32b4a16cfe22cb9567cfe80e
shaloo <5890484+shaloo@users.noreply.github.com>
2023-08-28 11:16:36 -0400

    Update Auth SDK to v1.0.8-beta.3

--- src/wallet.js
+++ src/wallet.js
@@ -10,7 +10,12 @@ let arcanaAuth = null;
 
 if (!arcanaAuth) {
   arcanaAuth = new AuthProvider(
-    "xar_dev_19527cdf585cd31d0bd06bfc1b008accea781404"
+    "xar_dev_19527cdf585cd31d0bd06bfc1b008accea781404",
+    {
+      connectOptions: {
+        compact: true
+      }
+    }
   );
 }
 
